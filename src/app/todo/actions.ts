700aae96ac57f0e2ba1e62969d88e6edcad1920b
eunji <dossiba0604@gmail.com>
2025-03-14 00:23:45 +0900

    ♻️ refactor : axios 인스턴스 baseURL 변경 및 코드 개선

--- src/app/todo/actions.ts
+++ src/app/todo/actions.ts
@@ -4,18 +4,18 @@ import axios from '@/lib/axios'
 import type { Todo } from '@/types/todo'
 
 export async function getTodos(): Promise<Todo[]> {
-  const response = await axios.get('/todos')
+  const response = await axios.get('/')
   return response.data
 }
 
 export async function addTodo(todo: Todo): Promise<void> {
-  await axios.post('/todos', todo)
+  await axios.post('/', todo)
 }
 
 export async function updateTodo(todo: Partial<Todo>): Promise<void> {
-  await axios.patch(`/todos/${todo.id}`, todo)
+  await axios.patch(`/${todo.id}`, todo)
 }
 
 export async function removeTodo(id: Todo['id']): Promise<void> {
-  await axios.delete(`/todos/${id}`)
+  await axios.delete(`/${id}`)
 }
