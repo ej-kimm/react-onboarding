cdf557d423b5ec60176c11d5b31059a450f91866
eunji <dossiba0604@gmail.com>
2025-03-13 17:15:57 +0900

    ✨ feat : Footer 컴포넌트 추가 및 스타일 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/common'
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
@@ -30,6 +31,7 @@ export default function RootLayout({
         <main className="bg-gradient flex min-h-screen w-screen flex-col items-center justify-center">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   )
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import github from '@/assets/images/github.jpg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-10 left-1/2 -translate-x-1/2 transform animate-[var(--animate-fade-in-up)_0.4s] opacity-0">
+      <div className="flex items-center gap-2">
+        <Link
+          href="https://github.com/ej-kimm/react-onboarding10.git"
+          target="_blank"
+        >
+          <Image
+            src={github}
+            width={44}
+            height={44}
+            alt="github"
+            style={{ width: '44px', height: '44px', borderRadius: '100%' }}
+          />
+        </Link>
+
+        <div className="flex flex-col">
+          <Link
+            className="text-white hover:underline"
+            href="https://github.com/ej-kimm/react-onboarding10.git"
+            target="_blank"
+          >
+            @ej-kimm
+          </Link>
+          <Link
+            className="text-white hover:underline"
+            href="mailto:dossiba0604@naver.com"
+          >
+            dossiba0604@naver.com
+          </Link>
+        </div>
+      </div>
+    </footer>
+  )
+}
