90db2a7cb826a10eacf59b3f912e3a77e6378b5b
eunji <dossiba0604@gmail.com>
2025-03-16 00:16:46 +0900

    💄 style : 커스텀 스크롤바 스타일 추가

--- src/app/todo/[filter]/layout.tsx
+++ src/app/todo/[filter]/layout.tsx
@@ -10,7 +10,7 @@ export default function FilteredTodoLayout({
     <div className="flex h-3/5 w-full max-w-[500px] flex-col overflow-hidden rounded-2xl shadow-2xl">
       <Header />
       <section className="bg-bg flex h-full min-h-0 flex-col">
-        <main className="my-2 flex-auto overflow-y-auto sm:my-5">
+        <main className="scrollbar my-2 flex-auto overflow-y-auto sm:my-5">
           {children}
         </main>
         <AddTodo />
