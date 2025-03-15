b5a187853c392614d3e0122e70e55998c4a8e6e4
eunji <dossiba0604@gmail.com>
2025-03-15 16:41:35 +0900

    🚀 chore : vercel.json 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const TODO_API_URL = 'http://localhost:3001/todos' // TODO : 임시 API 변경필요
+const TODO_API_URL = process.env.TODO_API_URL
 
 const instance = axios.create({
   baseURL: TODO_API_URL,
