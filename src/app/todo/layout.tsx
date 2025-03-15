5f6e3f8c40b038d81fde6fa54129f82a2f318ef7
eunji <dossiba0604@gmail.com>
2025-03-15 18:46:36 +0900

    🔥 chore : Vercel 404 오류 해결 전으로 되돌리기

--- src/app/todo/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function TodoLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <>{children}</>
-}
--- src/app/todo/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function TodoRootPage() {
-  redirect('/todo/all')
-}
