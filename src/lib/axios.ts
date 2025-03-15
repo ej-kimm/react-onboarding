e3843c71f4c68687fccd606f486342808436c7ac
eunji <dossiba0604@gmail.com>
2025-03-15 18:40:14 +0900

    🚀 fix : Vercel 배포 후 404 해결 - layout 컴포넌트명 오타 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const TODO_API_URL = 'https://melodious-cultured-nannyberry.glitch.me/todos'
+const TODO_API_URL = process.env.TODO_API_URL
 
 const instance = axios.create({
   baseURL: TODO_API_URL,
