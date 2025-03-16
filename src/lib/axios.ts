d249844f68c95bbad7dd42d4b549b1e6e8114b1a
eunji <dossiba0604@gmail.com>
2025-03-16 23:16:18 +0900

    🩹 chore : 환경 변수 없을 때 기본 API URL을 로컬로 설정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const TODO_API_URL = process.env.TODO_API_URL
+const TODO_API_URL = process.env.TODO_API_URL || 'http://localhost:3001/todos'
 
 const instance = axios.create({
   baseURL: TODO_API_URL,
